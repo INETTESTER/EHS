f60694da58c34745ddbd3cfd3ad26b4bc90cdb4b
Tester Inet Doc <tester.inet.doc@gmail.com>
2025-06-13 09:37:00 +0700

    q

--- open.sh
+++ open.sh
@@ -6,9 +6,9 @@
 ##########################################################################
                      API="template" 
                      google_sheet="https://docs.google.com/spreadsheets/d/1H7UgGtAy3JKvulLqGXVm5zeNq1FdNKonMxxBETQtjXQ/edit?gid=1610289956#gid=1610289956" 
-                     id="7"                 #เปลี่ยน id ทุกครั้งที่ยิง
-                     user="30000";            #จำนวนผู้ใช้งาน
-                     duration="2";         #วินาที
+                     id="1"                 #เปลี่ยน id ทุกครั้งที่ยิง
+                     user="1";            #จำนวนผู้ใช้งาน
+                     duration="1";         #วินาที
                      scenario="1"           #scenario="1" ยิงเเบบกำหนด request (duration ได้แค่ 1 วินาที)
                      cid="1"                #scenario="2" ยิงเเบบกำหนด VUs  (กำหนดว่า user x คน ใช้ระบบ x วินาที)
                                             #scenario="3" ยิงเเบบกำหนด request แต่ไม่แม่นยำ (duration กี่วินาทีก็ได้)
