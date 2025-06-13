343150b14e21f1592e82ee9c5b8a6ab4338a2700
Tester Inet Doc <tester.inet.doc@gmail.com>
2025-06-13 10:04:22 +0700

    a

--- api/ehp.js
+++ api/ehp.js
@@ -3,6 +3,6 @@ import http from 'k6/http';
 export function ehp() {
     const url = "https://loadcis.ehp.co.th/ehp/";
     const res = http.get(url);
-    console.log(res.body);
+    //console.log(res.body);
     return res
 }
